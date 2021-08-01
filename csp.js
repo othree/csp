b0e9041663a158ce87047d9a5a69c10ca98564ba
othree <othree@gmail.com>
2021-08-02 00:03:51 +0800

    Fix indent

--- csp.js
+++ csp.js
@@ -1,7 +1,7 @@
 const CSP = (directives) => {
   return directives
     .map((directive) => {
-    	return `${directive.name} ${directive.value.join(' ')};`;
+      return `${directive.name} ${directive.value.join(' ')};`;
     })
     .join(' ');
 };
